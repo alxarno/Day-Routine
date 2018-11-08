33728520d83bb11e475d544b6d813e9302769bbb
AlexeyArno <ledssssa@gmail.com>
2018-11-08 19:36:29 +0300

    Storage test done

--- src/storage/test/StorageKernelEmul/index.ts
+++ src/storage/test/StorageKernelEmul/index.ts
@@ -13,30 +13,69 @@ interface ITestParams{
 export default class SKEmul implements IStorageKernel {
   testParams:ITestParams
   testData:{[key:string]:Array<any>}
+  lastId:number=0
 
   constructor(_testParams:ITestParams, _testData:{[key:string]:Array<any>}){
     this.testParams = _testParams
     this.testData = _testData
+    
+    Object.keys(_testData).forEach(value=>{
+      _testData[value].forEach((item:{ID:number})=>{
+        if(item.ID>this.lastId) {
+          this.lastId = item.ID
+        }
+      })
+    })
   }
 
   public async Get(table:string, data?:any){
     await new Promise(resolve=>setTimeout(resolve, this.testParams.delay))
+    if(data){
+      let result:Array<any> = []
+      this.testData[table].forEach(currentValue=>{
+        let push:boolean = true
+        Object.keys(data).forEach(key=>{
+          if(data[key] !== currentValue[key]) push=false           
+        })
 
+        if(push) result.push(currentValue)
+      })
+      return Object.assign({},result)
+    }
+    return Object.assign({}, this.testData[table])
   }
 
   public async Insert(table:string, data:any){
     await new Promise(resolve=>setTimeout(resolve, this.testParams.delay))
-    return 0
+    data["ID"] = ++this.lastId
+    this.testData[table].push(data)
+    return this.lastId
   }
 
-  public async Update(table:string, data:any){
+  public async Update(table:string, data:{[key:string]:any}){
     await new Promise(resolve=>setTimeout(resolve, this.testParams.delay))
+    let done:boolean = false;
+    this.testData[table].forEach((currentValue:{ID:number}, index:number)=>{
+      if(done) return
+      if(data.ID == currentValue.ID){
+        let workValue:{[key:string]:any} = currentValue
 
+        Object.keys(data).forEach(key=>{
+          workValue[key] = data[key]
+        })
+
+        this.testData[table][index] = workValue
+        done = true;
+      }
+    })
   }
   
-  public async Delete(table:string, data:any){
+  public async Delete(table:string, data:{ID:number}){
     await new Promise(resolve=>setTimeout(resolve, this.testParams.delay))
-
+    this.testData[table] = this.testData[table].filter((v:{ID:number})=>{
+      if(v.ID == data.ID) return false
+      return true
+    })
   }
 
   public async TableCreate(name:string, schema:{[key:string]:Object}){
--- src/storage/test/storage.test.ts
+++ src/storage/test/storage.test.ts
@@ -1,7 +1,22 @@
 import { IStorageKernel } from "src/interfaces/storageKernel";
 import SKEmul from "./StorageKernelEmul";
+import { IStorage } from "src/interfaces/storage";
+import {Storage} from '../index'
 
 
+let warehouse:any = {
+  "statist":
+  [
+    {ID: 1, routineID:2, spent:"[0,0,0,2,0,1,3]", lastUpdate: new Date().getTime()},
+    {ID: 2, routineID:3, spent:"[0,1,0,2,3,1,3]", lastUpdate: new Date().getTime()}
+  ],
+  "routines":[
+    {ID: 1, actionBody: "https://localhost:8080", actionType: 2, colorScheme:"default", describe: "1 desc", hours: 12, name: "Task #1"}
+  ],
+  "dead_zones":[
+    {ID:1, name:"Yet", start: 0, done:11, enable: 0, disabled_days:"[]"}
+  ]    
+}
 
 let sk:IStorageKernel = new SKEmul(
   {
@@ -11,14 +26,90 @@ let sk:IStorageKernel = new SKEmul(
       query:false
     }
   },
-  {
-    "statist":
-    [
-      {ID: 1, routineID:2, spent:"[0,0,0,2,0,1,3]", lastUpdate: new Date().getTime()}
-    ]
-  }
+  warehouse
 )
+
+let storage:IStorage = new Storage(sk, ()=>{})
 
 test("Storage Create", ()=>{
   expect(sk).not.toBe(undefined)
+})
+
+test("Storage: Statistics Get", async()=>{
+  let result:Array<any> = await storage.Statistics().Get()
+  expect(result.length).toBe(warehouse["statist"].length)
+})
+
+test("Storage: Statistics Add", async()=>{
+  await storage.Statistics().Add({routineID:4, hours:4})
+  let rows:Array<any> = await storage.Statistics().Get()
+  expect(rows.length).toBe(warehouse["statist"].length++)
+})
+
+
+test("Storage: Statistics Delete", async()=>{
+  await storage.Statistics().Delete({ID:2})
+  let rows:Array<any> = await storage.Statistics().Get()
+  // 2 because we added new object early
+  expect(rows.length).toBe(2)
+})
+test("Storage: Routines Get", async()=>{
+  let result:Array<any> = await storage.Routines().Get()
+  expect(result.length).toBe(warehouse["routines"].length)
+})
+
+test("Storage: Routines Create", async()=>{
+  await storage.Routines().Create({
+    ID:0,
+    actionBody: "https://localhost:8080", 
+    actionType: 2,
+    colorScheme:"default",
+    describe: "2 desc",
+    hours: 14,
+    name: "Task #2"
+  })
+  let results:Array<any> = await storage.Routines().Get()
+  expect(results.length).toBe(2)
+})
+
+test("Storage: Routine Update", async()=>{
+  await storage.Routines().Update({
+    ID:1,
+    hours: 10
+  })
+  let rows:Array<any> = await storage.Routines().Get({ID: 1})
+  expect(rows[0]["hours"]).toBe(10)
+})
+
+test("Storage: Routine Delete", async()=>{
+  await storage.Routines().Delete({
+    ID: 1
+  })
+  let rows:Array<any> = await storage.Routines().Get()
+  // 1 because we added one more object to storage early
+  expect(rows.length).toBe(1)
+})
+
+test("Storage: DeadZone Get", async()=>{
+  let deadZones:Array<any> = await storage.DeadZones().Get()
+  expect(deadZones.length).toBe(1)
+})
+
+
+test("Storage: DeadZone Create", async()=>{
+  await storage.DeadZones().Create({ID:0, name:"Yet 2", start: 2, done:15, enable: true, disabled_days:[]})
+  let deadZones:Array<any> = await storage.DeadZones().Get()
+  expect(deadZones.length).toBe(2)
+})
+
+test("Stoarge: DeadZone Update", async()=>{
+  await storage.DeadZones().Update({ID:1, name:"Yet 25", start: 0, done:11, enable: 0, disabled_days:"[]"})
+  let deadZones:Array<any> = await storage.DeadZones().Get()
+  expect(deadZones[0]["name"]).toBe("Yet 25")
+})
+
+test("Storage: DeadZone Delete", async()=>{
+  await storage.DeadZones().Delete({ID:1})
+  let deadZones:Array<any> = await storage.DeadZones().Get()
+  expect(deadZones.length).toBe(1)
 })
