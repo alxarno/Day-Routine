196c12367bf471e29b03d0fc7b0ab3771fb99652
AlexeyArno <ledssssa@gmail.com>
2018-11-17 12:59:20 +0300

    Refactor dead_zones

---
+++ src/view/components/dead_zones/dead_zone/editor/index.ts
@@ -0,0 +1,101 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+import {DeadZone} from "src/models/dead_zone";
+import {Times, TimeHalf} from "./interfaces";
+import CircleComponent from "./circle";
+import SwitchComponent from "./switch-time-zone";
+import WeekComponent from "./week";
+
+import * as WithRender from "./template.html";
+import { Action } from "vuex-class";
+require("./styles.scss");
+
+const del = require("assets/rubbish-bin-delete-button.svg");
+
+const namespace: string = "deadZones";
+@WithRender
+@Component({
+  components: {
+    CircleComponent,
+    SwitchComponent,
+    WeekComponent,
+  },
+  props: {
+    zone: {
+      type: Object as () => DeadZone,
+    },
+    time: {
+      type: Object as () => Times,
+    },
+  },
+})
+export default class Editor extends Vue {
+  @Action("saveChangedDeadZone", { namespace }) public saveChangedDeadZone: any;
+  @Action("deleteDeadZone", { namespace }) public deleteDeadZone: any;
+
+  public startTimeZone: TimeHalf = TimeHalf.AM;
+  public doneTimeZone: TimeHalf = TimeHalf.AM;
+  public del: string = del;
+  public deleteSwitch: boolean = false;
+
+  public computed() {
+    this.startTimeZone = this.timeHalf(this.$props.zone.start);
+    this.doneTimeZone = this.timeHalf(this.$props.zone.done);
+  }
+
+  public triggeredRight(hour: number) {
+    if (this.doneTimeZone === TimeHalf.PM) {
+      hour += 12;
+    }
+    this.$props.zone.done = hour;
+  }
+
+  public trigerredLeft(hour: number) {
+    if (this.startTimeZone === TimeHalf.PM) {
+      hour += 12;
+    }
+    this.$props.zone.start = hour;
+  }
+
+  public changeZoneStart() {
+    if (this.$props.zone.start >= 12) {
+      this.$props.zone.start -= 12;
+      this.startTimeZone = TimeHalf.AM;
+    } else {
+      this.$props.zone.start += 12;
+      this.startTimeZone = TimeHalf.PM;
+    }
+  }
+
+  public changeZoneEnd() {
+    if (this.$props.zone.done >= 12) {
+      this.$props.zone.done -= 12;
+      this.doneTimeZone = TimeHalf.AM;
+    } else {
+      this.$props.zone.done += 12;
+      this.doneTimeZone = TimeHalf.PM;
+    }
+
+  }
+
+  public timeHalf(time: number): TimeHalf {
+    if (time >= 12) {
+      return TimeHalf.PM;
+    } else {
+      return TimeHalf.AM;
+    }
+  }
+
+  public deleteClick() {
+    if (this.deleteSwitch) {
+      this.deleteDeadZone(this.$props.zone);
+      return;
+    }
+    this.deleteSwitch = !this.deleteSwitch;
+  }
+
+  public beforeDestroy() {
+    this.saveChangedDeadZone(this.$props.zone);
+    // console.log("Destroy")
+  }
+}
---
+++ src/view/components/dead_zones/index.ts
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+import {State, Action} from "vuex-class";
+import * as WithRender from "./template.html";
+import DeadZoneComponent from "./dead_zone";
+import NewButton from "src/view/default-components/new_button/new_button";
+require("./styles.scss");
+
+const searchIcon = require("assets/search.svg");
+const namespace: string = "deadZones";
+
+@WithRender
+@Component({
+  components: {
+    DeadZoneComponent,
+    NewButton,
+  },
+})
+export default class DeadZones extends Vue {
+  @State("items", {namespace}) public deadZonesItems: any;
+  @State("currentItem", {namespace}) public currentItem: any;
+
+  @Action("setCurrentItem", { namespace }) public setCurrentItem: any;
+  @Action("loadDeadZones", { namespace }) public loadDeadZones: any;
+  @Action("newDeadZone", {namespace}) public newDeadZone: any;
+
+  public searchIcon: string = searchIcon;
+  public searchRequest: string = "";
+
+  public mounted() {
+    this.loadDeadZones();
+  }
+
+  public change(index: number) {
+    if (index === (this.currentItem as number)) {
+      this.setCurrentItem(-1);
+    } else {
+      this.setCurrentItem(index);
+    }
+  }
+
+  public newDeadZoneHandler(): void {
+    this.newDeadZone();
+  }
+}
