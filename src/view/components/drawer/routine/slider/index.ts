edf0e1cbaee3c022e9cc72162fc91b42d45a2451
AlexeyArno <ledssssa@gmail.com>
2018-11-18 14:19:43 +0300

    Fixed slider sensetivity and added click

--- src/view/components/drawer/routine/slider/index.ts
+++ src/view/components/drawer/routine/slider/index.ts
@@ -8,38 +8,20 @@ require("./styles.scss");
   props: {
     max: Number,
     now: Number,
+    min: Number,
   },
 })
 export default class SliderComponent extends Vue {
-
   public dragging: boolean = false;
-  public x: number = 0;
   public current: number = 1;
-  // MINIMAL_STEP =
-  public margin: number = 0;
 
   public startDrag() {
     this.dragging = true;
   }
-
   public doDrag(event: MouseEvent) {
     if (this.dragging) {
-      const avgX =  event.clientX - this.x;
-      this.margin += avgX;
-      // 3 is just index of speed
-      const minmargin = (((this.$refs.slider_element_ref) as HTMLElement).offsetWidth / this.$props.max) - 3;
-      if (this.margin >= minmargin && this.current < this.$props.max) {
-        this.current++;
-        this.margin = 0;
-        this.$emit("trigered", this.current);
-      } else if (this.margin <= -1 * minmargin && this.current > 1) {
-        if (this.current === 1) { return; }
-        this.current--;
-        this.margin = 0;
-        this.$emit("trigered", this.current);
-      }
+     this.action(event);
     }
-    this.x = event.clientX;
   }
 
   public stopDrag() {
@@ -50,6 +32,35 @@ export default class SliderComponent extends Vue {
     this.current = this.$props.now;
     document.addEventListener("mousemove", this.doDrag);
     window.addEventListener("mouseup", this.stopDrag);
+  }
+
+  public wrapperClick(event: MouseEvent) {
+    if ((event as any).path[0].getAttribute("data-click") === "target") {
+      this.action(event);
+    }
+  }
+
+  private action(event: MouseEvent) {
+    const el: HTMLElement = ((this.$refs.slider_element_ref) as HTMLElement);
+    const elWidth: number = el.offsetWidth;
+    const elLeft: number = this.getGlobalOffset(el);
+    const coeff: number = (elWidth / this.$props.max) - 0.5;
+    // If cursor leave slider X area
+    if (event.clientX > elLeft + elWidth || event.clientX  < elLeft) {return; }
+    let result: number = Math.floor((event.clientX - elLeft) / coeff);
+    if (result > this.$props.max) {result = this.$props.max; }
+    if (result < this.$props.min) {result = this.$props.min; }
+    this.current = result;
+    this.$emit("trigered", this.current);
+  }
+
+  private getGlobalOffset(el: HTMLElement) {
+    let x = 0;
+    while (el) {
+        x += el.offsetLeft;
+        el = (el.offsetParent as HTMLElement);
+    }
+    return x;
   }
 
 }
